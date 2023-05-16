6f750090e9a95cb46a02c48e41c3e4921a6b8929
guillodacosta <guillodiacosta@gmail.com>
2023-05-16 13:14:26 -0500

    program flow control added

--- programFlowControl.py
+++ programFlowControl.py
@@ -1,8 +1,9 @@
 name = input('write your name')
 print(f'name count{len(name)}')
+empty = len(name) == 0
 while len(name) == 0:
     name = input('your name is:')
-if name == 'Pablo':
+if name == 'Pablo' and name not in 'listanegrapabla'.casefold():
     print('we\'ll need to review your bags')
 elif name == 'Maria':
     print('please show us your passport')
